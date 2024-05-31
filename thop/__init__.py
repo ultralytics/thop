13e65a12b383c95292e187fca1119a6e6b802e19
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-05-31 21:59:30 +0200

    Fix version attribute (#15)
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- thop/__init__.py
+++ thop/__init__.py
@@ -1,9 +1,8 @@
-# from .onnx_profile import OnnxProfile
+__version__ = "0.2.3"
+
 import torch
 
 from .profile import profile, profile_origin
 from .utils import clever_format
 
 default_dtype = torch.float64
-
-__version__ = "0.2.1"
