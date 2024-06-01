e04e3171c177132e621a048a6660e13c2d4f935e
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-06-01 18:08:31 +0200

    Update README.md (#19)

--- benchmark/evaluate_famous_models.py
+++ benchmark/evaluate_famous_models.py
@@ -19,10 +19,13 @@ if torch.cuda.is_available():
     device = "cuda"
 
 for name in model_names:
-    model = models.__dict__[name]().to(device)
-    dsize = (1, 3, 224, 224)
-    if "inception" in name:
-        dsize = (1, 3, 299, 299)
-    inputs = torch.randn(dsize).to(device)
-    total_ops, total_params = profile(model, (inputs,), verbose=False)
-    print("%s | %.2f | %.2f" % (name, total_params / (1000**2), total_ops / (1000**3)))
+    try:
+        model = models.__dict__[name]().to(device)
+        dsize = (1, 3, 224, 224)
+        if "inception" in name:
+            dsize = (1, 3, 299, 299)
+        inputs = torch.randn(dsize).to(device)
+        total_ops, total_params = profile(model, (inputs,), verbose=False)
+        print("%s | %.2f | %.2f" % (name, total_params / (1000**2), total_ops / (1000**3)))
+    except Exception as e:
+        print(f"Warning: failed to process {e}")
