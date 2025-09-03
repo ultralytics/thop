8676338ec80c8b292d23234f4129c097c4fc603f
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-09-03 15:46:16 +0200

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- thop/__init__.py
+++ thop/__init__.py
@@ -1,8 +1,12 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-__version__ = "2.0.16"
+__version__ = "2.0.17"
 
+from .profile import profile, profile_origin
+from .utils import clever_format
 
 import torch
 
 default_dtype = torch.float64
+
+__all__ = ["profile", "profile_origin", "clever_format", "default_dtype"]
