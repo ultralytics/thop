d7c31a6cb4fd4bae97a21a000bd954b8acb92eec
UltralyticsAssistant <web@ultralytics.com>
2025-09-03 13:45:45 +0000

    Auto-format by https://ultralytics.com/actions

--- thop/vision/basic_hooks.py
+++ thop/vision/basic_hooks.py
@@ -4,10 +4,22 @@ import logging
 
 import torch
 import torch.nn as nn
-from thop.vision.calc_func import calculate_parameters, calculate_zero_ops, calculate_conv2d_flops, calculate_conv, \
-    calculate_norm, calculate_relu, calculate_relu_flops, calculate_softmax, calculate_avgpool, calculate_adaptive_avg, \
-    calculate_upsample, calculate_linear
 from torch.nn.modules.conv import _ConvNd
+
+from thop.vision.calc_func import (
+    calculate_adaptive_avg,
+    calculate_avgpool,
+    calculate_conv,
+    calculate_conv2d_flops,
+    calculate_linear,
+    calculate_norm,
+    calculate_parameters,
+    calculate_relu,
+    calculate_relu_flops,
+    calculate_softmax,
+    calculate_upsample,
+    calculate_zero_ops,
+)
 
 multiply_adds = 1
 
